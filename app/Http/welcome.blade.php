446807179094d07314dc3c24a069eec3e6a8998f
dkatakam <dkatakam@unomaha.edu>
2016-07-22 23:30:05 -0500

    I hope it works

--- app/Http/welcome.blade.php
+++ app/Http/welcome.blade.php
@@ -38,28 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-				<div align="center"><H1><FONT SIZE="6" FACE="arial" COLOR=blue>EAGLE FINANCIAL SERVICES </FONT></H1></div>
-				<div align="center"> <FONT SIZE="3" FACE="verdana" COLOR=red>YOUR MID WEST FINANCIAL SERVICES PARTNER</div>
-<div id="header1">
-	<!-- header1 Left -->
-	<div class="header1left">
-		<div class="logo">
-			<a href="http://localhost/eagle_final/public/"><h1><b>Home<b></h1></a>
-			
-		</div>
-
-		<nav class="menu">
-			<ul>
-				<li><a href="{{ action('CustomerController@index') }}">Customers</a></li>
-				<li><a href="{{ action('StockController@index') }}">Stocks</a></li>
-				<li><a href="{{ action('InvestmentController@index') }}">Investments</a></li>
-			</ul>
-		</nav>
-	</div>
-</div>
-
-<div class=""><img src="http://northwesternbusinessreview.org/wp-content/uploads/2016/05/finance-photo-.jpg" width="100%" height="600"></div>
-				
+                <div class="title">Laravel 5</div>
             </div>
         </div>
     </body>
