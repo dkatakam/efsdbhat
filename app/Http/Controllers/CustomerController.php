8469fe9baaea6241323f4f2de615d1ac0157ce45
dkatakam <dkatakam@unomaha.edu>
2016-07-25 23:37:07 -0500

    adding changes

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -67,13 +67,13 @@ class CustomerController extends Controller
         return redirect('customers');
     }
 
-   public function stringify($id)
-{
-    $customer = Customer::where('cust_number', $id)->select('cust_number','name','address','city','state','zip','home_phone','cell_phone')->first();
+    public function stringify($id)
+    {
+       // $customer=Customer::where('id', $id)->select('customer_id','name','address','city','state','zip','home_phone','cell_phone')->first();
+        $customer = Customer::where('cust_number', $id)->select('cust_number','name','address','city','state','zip','home_phone','cell_phone')->first();
 
-    $customer = $customer->toArray();
-    return response()->json($customer);
-}
-
+        $customer = $customer->toArray();
+        return response()->json($customer);
+    }
 
 }
